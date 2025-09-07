8bb68bba8686a28bf0a10259e7aa28db01acc30e
Ritej44 <ritejbouaicha@gmail.com>
2025-09-07 23:17:53 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-/* import { ServicesComponent } from './services/services.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { ContactComponent } from './contact/contact.component'; */
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // Default route (Home)
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
    { path: 'about', component: AboutComponent },
-  /*{ path: 'services', component: ServicesComponent },
-  { path: 'pricing', component: PricingComponent },
-  { path: 'contact', component: ContactComponent },*/
+   {
+    path:'login',component:LoginComponent
+   }
  ];
 
 @NgModule({
--- src/app/facture.service.ts
+++ src/app/facture.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Facture } from './Facture.model';
+import { Fournisseur } from './Fournisseur.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,10 @@ export class FactureService {
     return this.http.post<Facture>(this.apiUrl, facture);
   }
 
+  createFournisseur(fournisseur: Fournisseur): Observable<Fournisseur> {
+    return this.http.post<Fournisseur>(this.apiUrl, fournisseur);
+  }
+
   // Factures NON PAYÉES
   getFacturesNonPayees(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/non-payees`);
@@ -26,7 +31,7 @@ export class FactureService {
 
   // Factures PAYÉES
   getFacturesPayees(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/payees`);
+    return this.http.get<any[]>(`${this.apiUrl}/paye`);
   }
 
   // Par statut
@@ -40,10 +45,17 @@ export class FactureService {
     return this.http.get<Facture[]>(`${this.apiUrl}/fournisseur/${idFournisseur}`);
   }
 
-  payerFacture(id: string, notePaiement: string): Observable<Facture> {
-    return this.http.post<Facture>(`${this.apiUrl}/paiement/${id}`, notePaiement);
-  }
+  //payerFacture(id: string, notePaiement: string): Observable<Facture> {
+    //return this.http.post<Facture>(`${this.apiUrl}/paiement/${id}`, notePaiement);
+  //}
+  payerFacture(id: string, notePaiement: string, devise: string): Observable<Facture> {
+  return this.http.post<Facture>(`${this.apiUrl}/factures/${id}/payer`, { notePaiement, devise });
+}
 
+  private apiurl = 'http://localhost:8080/api/paiement';
+updatePaiement(id: string, paiement: any): Observable<any> {
+    return this.http.put(`${this.apiurl}/${id}`, paiement);
+  }
 
   updateFacture(id: string, facture: Facture): Observable<Facture> {
     return this.http.put<Facture>(`${this.apiUrl}/${id}`, facture);
